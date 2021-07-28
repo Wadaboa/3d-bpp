2748f5b8b68580b379589253533b03abe1db5b10
Alessio Falai <falai.alessio@gmail.com>
2021-07-28 21:55:28 +0200

    add maxrects with height groups in warm start / add logger

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # 3d-bpp
 
-Deps
-- brew install boost (1.75.0_3) boost-python3 (1.75.0) / sudo apt-get install libboost-all-dev
+python -m streamlit run src/dashboard.py
--- src/main.py
+++ src/main.py
@@ -1,127 +1,6 @@
+from loguru import logger
+
 import layers, superitems, config, maxrects, column_generation, baseline
-
-
-def baseline_procedure(order, tlim=None):
-    final_layer_pool = layers.LayerPool(superitems.SuperitemPool(), config.PALLET_DIMS)
-    working_order = order.copy()
-    for it in range(1):
-
-        superitems_pool = superitems.SuperitemPool(
-            superitems=superitems.SuperitemPool.gen_superitems(
-                order=working_order,
-                pallet_dims=config.PALLET_DIMS,
-                max_vstacked=4,
-                not_horizontal=True,
-            )
-        )
-
-        layer_pool = baseline.call_baseline_model(superitems_pool, config.PALLET_DIMS, tlim=tlim)
-        final_layer_pool.extend(layer_pool)
-
-        final_layer_pool = final_layer_pool.select_layers(min_density=0.5, max_coverage=3)
-
-        item_coverage = final_layer_pool.item_coverage()
-        not_covered = [k for k, v in item_coverage.items() if not v]
-        print(f"Items not covered: {len(not_covered)}/{len(item_coverage)}")
-        working_order = order.iloc[not_covered].copy()
-
-    return final_layer_pool
-
-
-def maxrect_procedure(order):
-    superitems_pool = superitems.SuperitemPool(
-        superitems=superitems.SuperitemPool.gen_superitems(
-            order=order,
-            pallet_dims=config.PALLET_DIMS,
-            max_vstacked=4,
-            not_horizontal=True,
-        )
-    )
-    final_layer_pool = layers.LayerPool(superitems_pool, config.PALLET_DIMS)
-    height_groups = get_height_groups(
-        superitems_pool, config.PALLET_DIMS, height_tol=50, density_tol=0.5
-    )
-    for i, spool in enumerate(height_groups):
-        print(f"Height group {i + 1}/{len(height_groups)}")
-        working_layer_pool = maxrects.maxrects_multiple_layers(
-            spool, config.PALLET_DIMS, add_single=False
-        )
-        final_layer_pool.extend(working_layer_pool)
-
-    print(len(final_layer_pool))
-
-    final_layer_pool = final_layer_pool.select_layers(
-        min_density=0.5,
-        two_dims=False,
-        max_coverage=3,
-        remove_duplicated=True,
-    )
-    print(len(final_layer_pool))
-    item_coverage = final_layer_pool.item_coverage()
-    not_covered = [k for k, v in item_coverage.items() if not v]
-    print(f"Items not covered: {len(not_covered)}/{len(item_coverage)}")
-
-    return final_layer_pool
-
-
-def column_generation_procedure(order, use_height_groups=True):
-    superitems_pool = superitems.SuperitemPool(
-        superitems=superitems.SuperitemPool.gen_superitems(
-            order=order,
-            pallet_dims=config.PALLET_DIMS,
-            max_vstacked=4,
-            not_horizontal=True,
-            only_single=True,
-        )
-    )
-    final_layer_pool = layers.LayerPool(superitems_pool, config.PALLET_DIMS, add_single=False)
-    bins_lbs = []
-
-    if use_height_groups:
-        height_groups = get_height_groups(
-            superitems_pool, config.PALLET_DIMS, height_tol=50, density_tol=0.5
-        )
-    else:
-        height_groups = [superitems_pool]
-
-    for i, spool in enumerate(height_groups):
-        print(f"Height group {i + 1}/{len(height_groups)}")
-        # TODO use height groups in warm start to work on more compact layers
-        # mr_layer_pool = layers.LayerPool(spool, config.PALLET_DIMS, add_single=True)
-        mr_layer_pool = maxrects.maxrects_multiple_layers(
-            spool, config.PALLET_DIMS, add_single=True
-        )
-        # num_mr_layers = len(mr_layer_pool) - len(spool)
-        working_layer_pool, bins_lb = column_generation.column_generation(
-            mr_layer_pool,
-            config.PALLET_DIMS,
-            max_iter=100,
-            # max_stag_iters=num_mr_layers + 1,
-            max_stag_iters=20,
-            tlim=5,
-            only_maxrect=False,
-            use_maxrect=False,
-            return_only_last=False,
-        )
-        bins_lbs.append(bins_lb)
-        final_layer_pool.extend(working_layer_pool)
-
-        # Add single item superitems that were discarded by
-        # the initial height groups procedure
-        filtered_layer_pool = final_layer_pool.select_layers(
-            min_density=0.5,
-            two_dims=False,
-            max_coverage=3,
-            remove_duplicated=True,
-        )
-
-        item_coverage = filtered_layer_pool.item_coverage()
-        print(item_coverage)
-        not_covered = [k for k, v in item_coverage.items() if not v]
-        print(not_covered)
-        print(f"Items not covered: {len(not_covered)}/{len(item_coverage)}")
-
-    return filtered_layer_pool, final_layer_pool, bins_lbs
 
 
 def get_height_groups(superitems_pool, pallet_dims, height_tol=0, density_tol=0.5):
@@ -155,5 +34,213 @@ def get_height_groups(superitems_pool, pallet_dims, height_tol=0, density_tol=0.
     return groups
 
 
-if __name__ == "__main__":
-    pass
+def maxrects_warm_start(superitems_pool, height_tol=0, density_tol=0.5):
+    """
+    Compute the warm start layer pool from maxrects, by calling the
+    maxrects procedure on each height group
+    """
+    logger.info("Computing warm start layers")
+
+    # Compute height groups and initialize initial layer pool
+    height_groups = get_height_groups(
+        superitems_pool, config.PALLET_DIMS, height_tol=height_tol, density_tol=density_tol
+    )
+    mr_layer_pool = layers.LayerPool(superitems_pool, config.PALLET_DIMS)
+
+    # Call maxrects for each height group and merge all the layer pools
+    for i, spool in enumerate(height_groups):
+        logger.info(f"Processing height group {i + 1}/{len(height_groups)}")
+        layer_pool = maxrects.maxrects_multiple_layers(spool, config.PALLET_DIMS, add_single=True)
+        mr_layer_pool.extend(layer_pool)
+    logger.info(f"Identified {len(mr_layer_pool)} layers in warm start")
+
+    # Return the final layer pool
+    return mr_layer_pool
+
+
+def baseline_procedure(
+    order,
+    max_iters=1,
+    density_tol=0.5,
+    filtering_two_dims=False,
+    filtering_max_coverage=3,
+    filtering_remove_duplicated=True,
+    baseline_tlim=None,
+):
+    """
+    Generate layers by calling the full-on mathematical programming approach
+    (with layers)
+    """
+    # Create the final superitems pool and a copy of the order
+    final_layer_pool = layers.LayerPool(superitems.SuperitemPool(), config.PALLET_DIMS)
+    working_order = order.copy()
+
+    # Iterate the specified number of times in order to reduce
+    # the number of uncovered items at each iteration
+    for iter in range(max_iters):
+        logger.info(f"Baseline iteration {iter + 1}/{max_iters}")
+
+        # Create the superitems pool and call the baseline procedure
+        superitems_pool = superitems.SuperitemPool(
+            superitems=superitems.SuperitemPool.gen_superitems(
+                order=working_order,
+                pallet_dims=config.PALLET_DIMS,
+                max_vstacked=4,
+                not_horizontal=True,
+            )
+        )
+        layer_pool = baseline.call_baseline_model(
+            superitems_pool, config.PALLET_DIMS, tlim=baseline_tlim
+        )
+        final_layer_pool.extend(layer_pool)
+
+        # Filter layers based on the given parameters
+        final_layer_pool = final_layer_pool.select_layers(
+            min_density=density_tol,
+            two_dims=filtering_two_dims,
+            max_coverage=filtering_max_coverage,
+            remove_duplicated=filtering_remove_duplicated,
+        )
+
+        # Compute the number of uncovered items
+        item_coverage = final_layer_pool.item_coverage()
+        not_covered = [k for k, v in item_coverage.items() if not v]
+        working_order = order.iloc[not_covered].copy()
+        logger.info(f"Items not covered: {len(not_covered)}/{len(item_coverage)}")
+
+    return final_layer_pool
+
+
+def maxrects_procedure(
+    order,
+    max_iters=1,
+    height_tol=0,
+    density_tol=0.5,
+    filtering_two_dims=False,
+    filtering_max_coverage=3,
+    filtering_remove_duplicated=True,
+):
+    """
+    Generate layers by calling maxrects on each height group and
+    merge everything in a single pool
+    """
+    # Create the final superitems pool and a copy of the order
+    final_layer_pool = layers.LayerPool(superitems.SuperitemPool(), config.PALLET_DIMS)
+    working_order = order.copy()
+
+    # Iterate the specified number of times in order to reduce
+    # the number of uncovered items at each iteration
+    for iter in range(max_iters):
+        logger.info(f"Maxrects iteration {iter + 1}/{max_iters}")
+
+        # Create the superitems pool and call the maxrects procedure
+        superitems_pool = superitems.SuperitemPool(
+            superitems=superitems.SuperitemPool.gen_superitems(
+                order=working_order,
+                pallet_dims=config.PALLET_DIMS,
+                max_vstacked=4,
+                not_horizontal=True,
+            )
+        )
+        layer_pool = maxrects_warm_start(
+            superitems_pool, height_tol=height_tol, density_tol=density_tol
+        )
+        final_layer_pool.extend(layer_pool)
+
+        # Filter layers based on the given parameters
+        final_layer_pool = final_layer_pool.select_layers(
+            min_density=density_tol,
+            two_dims=filtering_two_dims,
+            max_coverage=filtering_max_coverage,
+            remove_duplicated=filtering_remove_duplicated,
+        )
+
+        # Compute the number of uncovered items
+        item_coverage = final_layer_pool.item_coverage()
+        not_covered = [k for k, v in item_coverage.items() if not v]
+        working_order = order.iloc[not_covered].copy()
+        logger.info(f"Items not covered: {len(not_covered)}/{len(item_coverage)}")
+
+    # Return the final layer pool
+    return final_layer_pool
+
+
+def column_generation_procedure(
+    order,
+    use_height_groups=True,
+    height_tol=0,
+    density_tol=0.5,
+    warm_start=True,
+    cg_max_iters=100,
+    cg_max_stag_iters=20,
+    cg_tlim=5,
+    filtering_two_dims=False,
+    filtering_max_coverage=3,
+    filtering_remove_duplicated=True,
+):
+    """
+    Generate layers by calling the column generation procedure
+    """
+    # Create the superitems pool and the initial layer pool
+    superitems_pool = superitems.SuperitemPool(
+        superitems=superitems.SuperitemPool.gen_superitems(
+            order=order,
+            pallet_dims=config.PALLET_DIMS,
+            max_vstacked=4,
+            not_horizontal=True,
+            only_single=True,
+        )
+    )
+    layer_pool = layers.LayerPool(superitems_pool, config.PALLET_DIMS, add_single=False)
+
+    # Process superitems all together or by dividing them into height groups
+    if use_height_groups:
+        height_groups = get_height_groups(
+            superitems_pool, config.PALLET_DIMS, height_tol=height_tol, density_tol=density_tol
+        )
+    else:
+        height_groups = [superitems_pool]
+
+    # Iterate over each height group (or the entire superitems pool)
+    # and call the column generation procedure for each one
+    bins_lbs = []
+    for i, spool in enumerate(height_groups):
+        logger.info(f"Processing height group {i + 1}/{len(height_groups)}")
+
+        # Use either the warm start given by maxrects (over height groups)
+        # or a warm start comprised of one layer for each item
+        if warm_start:
+            warm_start_layer_pool = maxrects_warm_start(
+                spool, height_tol=height_tol, density_tol=density_tol
+            )
+        else:
+            warm_start_layer_pool = layers.LayerPool(spool, config.PALLET_DIMS, add_single=True)
+
+        # Call the column generation procedure
+        working_layer_pool, bins_lb = column_generation.column_generation(
+            warm_start_layer_pool,
+            config.PALLET_DIMS,
+            max_iter=cg_max_iters,
+            max_stag_iters=cg_max_stag_iters,
+            tlim=cg_tlim,
+            only_maxrect=False,
+            use_maxrect=False,
+            return_only_last=False,
+        )
+        bins_lbs.append(bins_lb)
+        layer_pool.extend(working_layer_pool)
+
+        # Filter layers based on the given parameters
+        layer_pool = layer_pool.select_layers(
+            min_density=density_tol,
+            two_dims=filtering_two_dims,
+            max_coverage=filtering_max_coverage,
+            remove_duplicated=filtering_remove_duplicated,
+        )
+
+        # Compute the number of uncovered items
+        item_coverage = layer_pool.item_coverage()
+        not_covered = [k for k, v in item_coverage.items() if not v]
+        logger.info(f"Items not covered: {len(not_covered)}/{len(item_coverage)}")
+
+    return layer_pool, bins_lbs
